37a388f39532ccfbce0d97cdb2a8f5f6a6216fec
Gustavo Cainã Branco <cainabranco@gmail.com>
2020-07-30 13:18:27 -0300

    first deploy commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import Menu from "./componets/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./componets/BannerMain";
+import Carousel from "./componets/Carousel";
+import Footer from "./componets/Footer";
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+      <div style={{ background: "#141414" }}>
+          <Menu />
+
+          <BannerMain
+              videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+              url={dadosIniciais.categorias[0].videos[0].url}
+              videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+          />
+
+          <Carousel
+              ignoreFirstVideo
+              category={dadosIniciais.categorias[0]}
+          />
+
+          <Carousel
+              category={dadosIniciais.categorias[1]}
+          />
+
+          <Carousel
+              category={dadosIniciais.categorias[2]}
+          />
+
+          <Carousel
+              category={dadosIniciais.categorias[3]}
+          />
+
+          <Carousel
+              category={dadosIniciais.categorias[4]}
+          />
+
+          <Carousel
+              category={dadosIniciais.categorias[5]}
+          />
+
+          <Footer />
+      </div>
   );
 }
 
